112105955cbdd2bda96348354bde69e37aee8fea
thuyhangle <thuyhangle.le@gmail.com>
2019-05-28 03:21:34 +0300

    Sample data list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render () {
     const helloWorld = 'Welcome to ReactJS';
