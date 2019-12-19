0be6afa27e85e73c16f4b2f15ced1b8c8f704d37
thuyhangle <thuyhangle.le@gmail.com>
2019-12-19 20:31:54 +0200

    Search on server-side

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-function isSearched(searchTerm) {
-  return function (item) {
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }
-}
-
 class App extends Component {
 
   constructor(props) {
@@ -23,7 +17,9 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
     };
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onClickMe = this.onClickMe.bind(this);
   }
@@ -32,13 +28,16 @@ class App extends Component {
     this.setState({ result });
   }
 
-  componentDidMount() {
-    const { searchTerm } = this.state;
-
+  fetchSearchTopStories(searchTerm) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onClickMe() {
@@ -47,6 +46,12 @@ class App extends Component {
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
   }
 
   onDismiss(id) {
@@ -84,6 +89,7 @@ class App extends Component {
             <Search
               value={searchTerm}
               onChange={this.onSearchChange}
+              onSubmit={this.onSearchSubmit}
             >
               Search
             </Search>
@@ -91,46 +97,35 @@ class App extends Component {
           { result
             ? <Table
               list={result.hits}
-              pattern={searchTerm}
               onDismiss={this.onDismiss}
             />
             :null
           }
-          <div className="Binding">
-            <h3>
-              <span>Binding</span>
-              <button
-                onClick={this.onClickMe.bind(this)}
-                type="button"
-                >
-                Click Me - Binding button - show log
-              </button>
-            </h3>
-
-          </div>
-
         </header>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) =>
+const Search = ({ value, onChange, onSubmit, children }) =>
   <div className="Forms">
-    <form>
+    <form onSubmit={onSubmit}>
       <label>{children} </label>
       <input
         type="text"
         value={value}
         onChange={onChange}
       />
+      <button type="submit">
+        {children}
+      </button>
     </form>
   </div>
 
-const Table = ({ list, pattern, onDismiss}) =>
+const Table = ({ list, onDismiss}) =>
   <div className="table">
     <h3>List of items</h3>
-    {list.filter(isSearched(pattern)).map(item => {
+    {list.map(item => {
       return (
         <div key={item.objectID} className="table-row">
           <span style={{ width: '40%' }}>
